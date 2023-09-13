563aab6d93c3910a444759e05c83224cd82bcea9
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-09-12 21:27:20 -0400

    Create pco_request_token.php

---
+++ pco_request_token.php
@@ -0,0 +1,25 @@
+<?php
+    #$pco_secret = '';
+    #$pco_client_id = '';
+    $pco_uri = 'http://127.0.0.1:8888/index.html';
+
+    $pco_code = json_decode(file_get_contents('php://input'), true);
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, 'https://api.planningcenteronline.com/oauth/token');
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, [
+        'grant_type' => 'authorization_code',
+        'code' => "{$pco_code}",
+        'client_id' => "{$pco_client_id}",
+        'client_secret' => "{$pco_secret}",
+        'redirect_uri' => "{$pco_uri}",
+    ]);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+    echo "{$response}";
+?>
+
+
