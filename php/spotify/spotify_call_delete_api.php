d8ac6a7028d9996625a575271ec7c0c18b913cf9
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-09-26 13:22:06 -0400

    Update spotify_call_delete_api.php

--- php/spotify/spotify_call_delete_api.php
+++ php/spotify/spotify_call_delete_api.php
@@ -1,33 +1,28 @@
 <?php
     $post_info = json_decode(file_get_contents('php://input'), true);
-    $spotify_song_uri = $post_info['spotify_song_uri'];
-    $spotify_playlist_id = $post_info['spotify_snapshot_id'];
-
-    $post_data = '{
-        "tracks": [
-            {
-                "uri": "' . $spotify_song_uri . '"
-            }
-        ],
-        "snapshot_id": "' . $spotify_playlist_id . '"
-    }';
-
-    echo $post_data;
-
     $spotify_url = $post_info["url"];
     $spotify_access_token = $post_info["spotify_access_token"];
-    $spotify_method = $post_info['spotify_request_method'];
+
+    echo json_encode($post_info['test']);
+
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $spotify_url);
     curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-    curl_setopt($ch, CURLOPT_POST, true);
-    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, $spotify_method);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'DELETE');
     curl_setopt($ch, CURLOPT_HTTPHEADER, [
         'Content-Type: application/json',
         "Authorization: Bearer {$spotify_access_token}",
     ]);
-    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_data));
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($post_info['test']));
 
-    $response = json_encode(curl_exec($ch));
-    echo $response;
+    $response = curl_exec($ch);
+
+    if($response !== false) {
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if($httpcode == 200) {
+            echo json_encode($response);
+        } elseif($httpcode == 400) {
+            echo $response;
+        }
+    }
 ?>
