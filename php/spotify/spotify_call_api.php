ca0fdd6ab0af9e339b1cf317f498d1f65b8ebf64
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-10-02 15:32:39 -0400

    Update spotify_call_api.php

--- php/spotify/spotify_call_api.php
+++ php/spotify/spotify_call_api.php
@@ -2,7 +2,7 @@
     $post_info = json_decode(file_get_contents('php://input'), true);
     $spotify_url = $post_info["url"];
     $spotify_access_token = $post_info["spotify_access_token"];
-    $spotify_method = $post_info['spotify_request_method'];
+    $spotify_method = $post_info['method'];
 
     $ch = curl_init();
     curl_setopt($ch, CURLOPT_URL, $spotify_url);
@@ -13,16 +13,14 @@
         "Authorization: Bearer {$spotify_access_token}",
     ]);
 
-
     $response = curl_exec($ch);
     if($response !== false) {
         $http_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         if($http_code == 200) {
             echo json_encode($response);        
         } elseif($http_code == 401) {
-            $response = '{"error": "401"}';
+            $response = '401';
             echo json_encode($response);        
         }
     }
-    
 ?>
