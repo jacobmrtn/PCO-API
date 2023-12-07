00edf0372dda092a7181d27394c9c21a1571e2f0
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-12-07 12:42:23 -0500

    Delete php/pco/pco_init_oauth.php

--- php/pco/pco_init_oauth.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    //$pco_secret = '';
-    //$pco_client_id = '';
-    $pco_uri = 'http://127.0.0.1:8888/app.html';
-    $scope = 'services';
-    
-    header("Location: https://api.planningcenteronline.com/oauth/authorize?client_id={$pco_client_id}&redirect_uri={$pco_uri}&response_type=code&scope={$scope}");
-    exit;
-?>
