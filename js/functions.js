95e36dd33280f27d2eb4f7ce34c7f39ac2dcdc80
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-12-07 12:40:12 -0500

    Added refresh token functions for Spotify and PCO

--- js/functions.js
+++ js/functions.js
@@ -11,6 +11,13 @@ function loading_text(id, text, timeout, type) {
             element.innerHTML = ''
             element.removeAttribute('class', 'success-text')
         }, timeout)
+    } else if(type === 'refresh') {
+        element.setAttribute('class', 'refresh-text')
+        element.innerText = text
+        setTimeout(() => {
+            element.innerHTML = ''
+            element.removeAttribute('class', 'refresh-text')
+        }, timeout)
     }
 
 }
@@ -39,4 +46,60 @@ async function spotify_call_api(method, url) {
     return response.json()
 }
 
-export { loading_text, remove_all_children, spotify_call_api }
+function spotify_refresh_access_token() {
+    const request_info = JSON.stringify({
+        "spotify_refresh_token": localStorage.getItem('spotify_refresh_token'),
+    })
+    fetch('./php/spotify/spotify_refresh_access_token.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: request_info
+    })        
+        .then(response => response.json())
+        .then(data => {
+            data = JSON.parse(data)
+            if(data.access_token) {
+                localStorage.setItem('spotify_access_token', data.access_token)
+                loading_text('spotify_loading_text', 'Successfully refreshed token!', 5000, 'success')
+            } else if(data.refresh_token) {
+                localStorage.setItem('spotify_access_token', data.access_token)
+                localStorage.setItem('spotify_refresh_token', data.refresh_token)
+                loading_text('spotify_loading_text', 'Successfully refreshed token!', 5000, 'success')
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+function pco_refresh_access_token() {
+    const request_info = JSON.stringify({
+        "pco_refresh_token": localStorage.getItem('pco_refresh_token')
+    })
+
+    fetch('./php/pco/pco_refresh_access_token.php', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: request_info
+    }) 
+        .then(response => response.json())
+        .then(data => {
+            data = JSON.parse(data)
+            if(data.access_token) {
+                loading_text('pco_loading_text', 'Successfully refreshed token!', 5000, 'success')
+                localStorage.setItem('pco_access_token', data.access_token) 
+                localStorage.setItem('pco_refresh_token', data.refresh_token)
+            } else {
+                loading_text('pco_loading_text', 'Access token expired - Requst a new one!', null, 'error-text')
+            }
+        })
+        .catch(error => {
+            console.error(error)
+        })
+}
+
+export { loading_text, remove_all_children, spotify_call_api, spotify_refresh_access_token, pco_refresh_access_token }
