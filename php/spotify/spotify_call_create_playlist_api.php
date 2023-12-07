ae24c83b8b2ddf6533b3882adf732ec245ffdccc
jacobmrtn <135056345+jacobmrtn@users.noreply.github.com>
2023-12-07 12:44:25 -0500

    Create spotify_call_create_playlist_api.php

---
+++ php/spotify/spotify_call_create_playlist_api.php
@@ -0,0 +1,30 @@
+<?php
+    $post_info = json_decode(file_get_contents('php://input'), true);
+    $spotify_url = $post_info["url"];
+    $spotify_access_token = $post_info["spotify_access_token"];
+    $spotify_playlist_data = $post_info["playlist_data"];
+
+
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL, $spotify_url);
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_HTTPHEADER, [
+        'Content-Type: application/json',
+        "Authorization: Bearer {$spotify_access_token}", 
+    ]);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($spotify_playlist_data));
+
+    $response = curl_exec($ch);
+
+    if($response !== false) {
+        $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+        if($httpcode == 201) {
+            echo json_encode($response);
+        } elseif($httpcode == 400) {
+            echo json_encode($response);
+        } else {
+            echo json_encode($response);
+        }
+    }
+?>
